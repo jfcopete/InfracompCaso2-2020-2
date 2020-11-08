f74925953a11a92a321c4f59c63f349a0205a6bc
JuanFCopete <jf.copete@uniandes.edu.co>
2020-11-08 15:28:53 -0500

    SHA-256 y MD5 funcionando

--- src/main/main.java
+++ src/main/main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -26,11 +27,34 @@ public class main {
 		}
 	}
 	
+	public static byte[] getSHA256(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			return md.digest(input.getBytes(StandardCharsets.UTF_8));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static String bytesToHex(byte[] hash ) {
+		BigInteger number = new BigInteger(1,hash);
+		StringBuilder hexString = new StringBuilder(number.toString(16));
+		while (hexString.length()<32) {
+			hexString.insert(0, '0');
+		}
+		return hexString.toString();
+		
+	}
+	
 	public static String generar_codigo(String cadena,String alg)
 	{
 		String codigo="";
 		if (alg.equals("MD5")) {
 			codigo = getMd5(cadena);
+		}
+		else if (alg.equals("SHA-256")) {
+			codigo = bytesToHex(getSHA256(cadena));
 		}
 		return codigo;
 	}
@@ -42,6 +66,7 @@ public class main {
 		BufferedReader br = new BufferedReader(isr);
 		System.out.println("Escriba el nombre del algoritmo :");
 		System.out.println("1: MD5");
+		System.out.println("2: SHA-256");
 		String alg = br.readLine();
 		System.out.println("Intregar Cadena");
 		String cadena = br.readLine();
