d8a3de3fb3fa4094c2ea9813c6ca64b171634aee
JuanFCopete <jf.copete@uniandes.edu.co>
2020-11-08 15:36:55 -0500

    SHA-384 Funcionando

--- src/main/main.java
+++ src/main/main.java
@@ -11,6 +11,11 @@ import java.security.NoSuchAlgorithmException;
 
 public class main {
 	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 */
 	public static String getMd5(String input)
 	{
 		try {
@@ -27,6 +32,11 @@ public class main {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 */
 	public static byte[] getSHA256(String input) {
 		try {
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -37,6 +47,22 @@ public class main {
 		return null;
 	}
 	
+	public static String getSHA384(String input) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-384");
+		byte[] messageDigest = md.digest(input.getBytes());
+		BigInteger no = new BigInteger(1, messageDigest);
+		String hastext = no.toString(16);
+		while (hastext.length() < 32) {
+			hastext= "0"+hastext;
+		}
+		return hastext;
+	}
+	
+	/**
+	 * 
+	 * @param hash
+	 * @return
+	 */
 	public static String bytesToHex(byte[] hash ) {
 		BigInteger number = new BigInteger(1,hash);
 		StringBuilder hexString = new StringBuilder(number.toString(16));
@@ -47,7 +73,14 @@ public class main {
 		
 	}
 	
-	public static String generar_codigo(String cadena,String alg)
+	/**
+	 * 
+	 * @param cadena
+	 * @param alg
+	 * @return
+	 * @throws NoSuchAlgorithmException 
+	 */
+	public static String generar_codigo(String cadena,String alg) throws NoSuchAlgorithmException
 	{
 		String codigo="";
 		if (alg.equals("MD5")) {
@@ -56,9 +89,17 @@ public class main {
 		else if (alg.equals("SHA-256")) {
 			codigo = bytesToHex(getSHA256(cadena));
 		}
+		else if (alg.equals("SHA-384")) {
+			codigo = getSHA384(cadena);
+		}
 		return codigo;
 	}
 
+	/**
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
@@ -67,12 +108,19 @@ public class main {
 		System.out.println("Escriba el nombre del algoritmo :");
 		System.out.println("1: MD5");
 		System.out.println("2: SHA-256");
+		System.out.println("2: SHA-384");
 		String alg = br.readLine();
 		System.out.println("Intregar Cadena");
 		String cadena = br.readLine();
 		System.out.println("El código criptografico hash para la cadena segun el algoritmo "+ alg +" es: ");
 		System.out.println(""+cadena+" "+alg);
-		String  output = generar_codigo(cadena, alg);
+		String output="";
+		try {
+			output = generar_codigo(cadena, alg);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println(" "+output);
 		
 		
